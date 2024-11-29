dbcf2af02235438be48b53f3bdf929aa97f82163
yoonsunny17 <jyoonsun0217@gmail.com>
2024-11-29 20:03:27 +0900

    [style] theme controller, custom color 적용

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#D91656",
+        sub1: "#FFB200",
+      },
+    },
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark"],
+  },
 };
