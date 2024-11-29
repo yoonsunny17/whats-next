b761f44cb10988abd44c38453fff9db5105c8fe2
yoonsunny17 <jyoonsun0217@gmail.com>
2024-11-29 15:46:08 +0900

    [feat] Layout 컴포넌트 구현
    - Theme Controller 구현
    - Header 구현
    - 렌더링 중일 때 Loading 컴포넌트 구현

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./routes/Home";
-import Detail from "./routes/Detail";
+import { MovieDetail } from "./routes/MovieDetail";
+import "../src/index.css";
+import { Layout } from "./components/Layouts/Layout";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/movie/:id" element={<Detail />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+      </Route>
+      <Route path="/movie/:id" element={<MovieDetail />} />
     </Routes>
   );
 }
